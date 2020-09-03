14f063bbca69b5a00166c40f858504e79e062e67
MelissaTanksley <melissa.tanksley@gmail.com>
2020-09-03 16:02:01 -0700

    main page and staff cards complete

--- GitHubCard/index.js
+++ GitHubCard/index.js
@@ -4,6 +4,11 @@
     https://api.github.com/users/<your name>
 */
 
+axios.get('https://api.github.com/users/MelissaTanksley').then((response) => {
+	console.log(response);
+	comp(response.data);
+});
+
 /*
   STEP 2: Inspect and study the data coming back, this is YOUR
     github info! You will need to understand the structure of this
@@ -28,7 +33,25 @@
     user, and adding that card to the DOM.
 */
 
-const followersArray = [];
+const cards = document.querySelector('.cards');
+
+const followersArray = [
+    'tetondan',
+    'thetaylorjacobs',
+    'justsml',
+    'dustinmyers',
+    'bigknell'
+];
+
+followersArray.forEach((user, item) =>
+    axios.get(`https://api.github.com/users/${followersArray[item]}`)
+    .then(data => {
+        const cardInfo = Card(data.data);
+        cards.appendChild(cardInfo)
+    })
+    .catch(error => console.log('ERORR OCCURED: ', error))
+)
+
 
 /*
   STEP 3: Create a function that accepts a single object as its only argument.
@@ -50,6 +73,58 @@ const followersArray = [];
     </div>
 */
 
+
+function Card(Obj) {
+
+  const cardDiv = document.createElement('div')
+  const image = document.createElement('img')
+  const cardInfoDiv = document.createElement('div')
+  const h3Name = document.createElement('h3')
+  const pUsername = document.createElement('p')
+  const pLocation = document.createElement('p')
+  const pProfile = document.createElement('p')
+  const aProfile = document.createElement('a')
+  const pFollowers = document.createElement('p')
+  const pFollowing = document.createElement('p')
+  const pBio = document.createElement('p')
+
+  cardDiv.classList.add('card')
+  cardInfoDiv.classList.add('card-info')
+  h3Name.classList.add('name')
+  pUsername.classList.add('username')
+
+  cardDiv.appendChild(image)
+  cardDiv.appendChild(cardInfoDiv)
+  cardInfoDiv.appendChild(h3Name)
+  cardInfoDiv.appendChild(pUsername)
+  cardInfoDiv.appendChild(pLocation)
+  cardInfoDiv.appendChild(pProfile)
+  cardInfoDiv.appendChild(pFollowers)
+  cardInfoDiv.appendChild(pFollowing)
+  cardInfoDiv.appendChild(pBio)
+  pProfile.appendChild(aProfile)
+
+  image.src = Obj.avatar_url
+  h3Name.textContent = Obj.name
+  pUsername.textContent = Obj.login
+  pLocation.textContent = `Location: ${Obj.location}`
+  aProfile.textContent = `Profile: ` + Obj.html_url
+  pFollowers.textContent = `Followers: ${Obj.followers}`
+  pFollowing.textContent = `Following: ${Obj.following}`
+  pBio.textContent = `Bio: ${Obj.bio}`
+
+  return cardDiv
+} 
+
+
+
+
+
+
+
+
+
+
 /*
   List of LS Instructors Github username's:
     tetondan
@@ -58,3 +133,4 @@ const followersArray = [];
     luishrd
     bigknell
 */
+
